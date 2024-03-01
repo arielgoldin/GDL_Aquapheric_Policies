76a53bd9cbf8ba64a0859190c0e720a24d8b939e
arielgoldin <arielgoldinm@gmail.com>
2024-03-01 14:51:39 +0100

    Update
    Solved issues with negative available water, outcomes, different formulations. Currently runs for a single drought scenario with two different formulations hard coded.

--- AMG_function.py
+++ AMG_function.py
@@ -1,77 +1,142 @@
 import math
 import numpy as np
 import pandas as pd
+#import rbf_functions
+from platypus import EpsNSGAII, ProcessPoolEvaluator
+#import rbf_functions
 
-'''import sysprivate_intakes_1
+'''import sysdomestic_intakes_PP1
 
-supply_areas=pd.read_csv("data/supply_areas.csv")
+supplied_areas=pd.read_csv("data/supplied_areas.csv")
 '''
 
+def supplied_demand (outcomes, ZA):
+    supplied_demand_value = outcomes[f"supplied_{ZA}"] / outcomes[f"demand_{ZA}"]
 
-def AMG_model(private_intakes_1=445436, private_intakes_2=125446, private_intakes_3=147032, private_intakes_4=128862, private_intakes_5=199613
-              ,public_consumption=129 #l/day/inhabitant
-              ,crowding_factor = 3.5 #people per household
+    return supplied_demand_value
+
+def sq_deficit (outcomes, ZA):
+      sq_deficit_value = (outcomes[f"supplied_{ZA}"] - outcomes[f"demand_{ZA}"])**2
+
+      return sq_deficit_value
+
+
+def AMG_model(domestic_intakes_PP1=445436, domestic_intakes_PP2=125446, domestic_intakes_PP3=147032, domestic_intakes_Toluquilla=128862, domestic_intakes_Pozos=199613
+              ,domestic_consumption=127 #l/day/inhabitant
+              ,crowding_factor = 3.55 #people per household
               ,chapala_flow = 6.9, calderon_flow = 1, zapotillo_flow = 1, pozos_flow = 2.3, toluquilla_flow = 0.5
-              ,node_division = 0.35
+              ,chapalaPP1_to_chapalaPP2 = 0.19 #Proportion of water that is sent from the supplied line of PP1 to PP2
               ,loss_grid = 0.35 # in %
               ,loss_potabilisation = 0.07 # losses in potabilization and transport in %
-              , mfc_flow_1 = 0, mfc_flow_2 = 0, mfc_flow_3 = 0, mfc_flow_4 = 0, mfc_flow_5 = 0
+              ,aqp4_Toluquilla_to_PP1 = 0, aqp1_PP2_to_PP3 = 0, aqp2_PP3_to_Pozos = 0, aqp3_Pozos_to_Toluquilla = 0
               ,rainfall = 80 #mm/month
               ,harvest_coeficient = 0.75 #proportion of water that falls on rooftop that can be harvested
               ,average_household_area = 80 #m2 
-              ,rfh_proportion_1 = 0.1, rfh_proportion_2 = 0.3, rfh_proportion_3 = 1.1, rfh_proportion_4 = 0.3, rfh_proportion_5 = 0.1 #proportion of the houses that have rfc /100 
+              ,rfh_proportion_PP1 = 0.1, rfh_proportion_PP2 = 0.3, rfh_proportion_PP3 = 1.1, rfh_proportion_Toluquilla = 0.3, rfh_proportion_Pozos = 0.1 #proportion of the houses that have rfc /100 
               ):
+        
+
+        ZA_names = ["PP1", "PP2", "PP3", "Toluquilla", "Pozos"]
+        model_vars = ["demand", "delivered", "potabilized", "supplied"]
+        
+
+
+
+        
         
         #1. DEMAND
 
         #1.1 Household demand
         conversion_m3s = 1/(24*60*60*1000) #converts l/day into m3/s
-        demand1 = private_intakes_1 * crowding_factor * public_consumption * conversion_m3s
-        demand2 = private_intakes_2 * crowding_factor * public_consumption * conversion_m3s
-        demand3 = private_intakes_3 * crowding_factor * public_consumption * conversion_m3s
-        demand4 = private_intakes_4 * crowding_factor * public_consumption * conversion_m3s
-        demand5 = private_intakes_5 * crowding_factor * public_consumption * conversion_m3s
-        total_demand = demand1 + demand2 + demand3 + demand4 + demand5
+
+        demand_PP1 = domestic_intakes_PP1 * crowding_factor * domestic_consumption * conversion_m3s
+        demand_PP2 = domestic_intakes_PP2 * crowding_factor * domestic_consumption * conversion_m3s
+        demand_PP3 = domestic_intakes_PP3 * crowding_factor * domestic_consumption * conversion_m3s
+        demand_Toluquilla = domestic_intakes_Toluquilla * crowding_factor * domestic_consumption * conversion_m3s
+        demand_Pozos = domestic_intakes_Pozos * crowding_factor * domestic_consumption * conversion_m3s
+
+        total_demand = demand_PP1 + demand_PP2 + demand_PP3 + demand_Toluquilla + demand_Pozos
+
+        demand_outputs = {"demand_PP1": demand_PP1,"demand_PP2": demand_PP2,"demand_PP3": demand_PP3,"demand_Toluquilla": demand_Toluquilla,"demand_Pozos": demand_Pozos}
+        #demand_outputs = {f"demand_{ZA}": globals()[f"demand_{ZA}"] for ZA in ZA_names} #!add total demand later
+
+        
+
+        #2. EXTRACTION
+        total_extraction = chapala_flow + calderon_flow + toluquilla_flow + pozos_flow
+
+        #DELIVERED
+        delivered_PP1 = chapala_flow * (1-chapalaPP1_to_chapalaPP2)
+        delivered_PP2 = chapala_flow * chapalaPP1_to_chapalaPP2
+        delivered_PP3 = calderon_flow + zapotillo_flow
+        delivered_Pozos = pozos_flow
+        delivered_Toluquilla = toluquilla_flow
+
+        delivered_outputs = {"delivered_PP1": delivered_PP1,"delivered_PP2": delivered_PP2,"delivered_PP3": delivered_PP3,"delivered_Toluquilla": delivered_Toluquilla,"delivered_Pozos": delivered_Pozos}
+        
+        
       
         
-        #2. SUPPLY
-        supply_2 = (chapala_flow * node_division * (1-loss_potabilisation) - mfc_flow_1) * (1-loss_grid)
-        supply_3 = ((calderon_flow + zapotillo_flow ) * (1-loss_potabilisation) + mfc_flow_1 - mfc_flow_2) * (1- loss_grid) 
-        supply_5 = (pozos_flow  + mfc_flow_2 - mfc_flow_3 ) * (1-loss_grid)
-        supply_4 = (toluquilla_flow * (1-loss_potabilisation) + mfc_flow_3 - mfc_flow_4) * (1-loss_grid)
-        supply_1 = (chapala_flow * (1-node_division) * (1-loss_potabilisation) + mfc_flow_4) * (1-loss_grid)
-        total_supply = supply_1 + supply_2 + supply_3 + supply_4 + supply_5
+        #2. Water potabilized in each supplied area OF WATER AND ITS DISTRIBUTION TO THE supplied AREAS
+        potabilized_PP1 = (delivered_PP1 * (1-loss_potabilisation))
+        potabilized_PP2 = (delivered_PP2* (1-loss_potabilisation))
+        potabilized_PP3 = (delivered_PP3* (1-loss_potabilisation))
+        potabilized_Toluquilla = (delivered_Toluquilla * (1-loss_potabilisation))
+        potabilized_Pozos = delivered_Pozos #the flow from the wells doesn't present any loss as it is only chlorinated (not potabilized) before entering the grid
+
+        potabilized_outputs = {"potabilized_PP1": potabilized_PP1,"potabilized_PP2": potabilized_PP2,"potabilized_PP3": potabilized_PP3,"potabilized_Toluquilla": potabilized_Toluquilla,"potabilized_Pozos": potabilized_Pozos}
+        
+        #ADDITIONAL FLOWS
+        additional_flow_PP1 = potabilized_PP1 * -0.12
+        additional_flow_PP2 = 0
+        additional_flow_PP3 = potabilized_Pozos * 0.2
+        additional_flow_Toluquilla = (potabilized_PP1 * 0.12 + potabilized_Pozos * 0.02)
+        additional_flow_Pozos = potabilized_Pozos * -0.22
+
+        additional_flow_outputs = {"additional_flow_PP1": additional_flow_PP1,"additional_flow_PP2": additional_flow_PP2,"additional_flow_PP3": additional_flow_PP3,"additional_flow_Toluquilla": additional_flow_Toluquilla,"additional_flow_Pozos": additional_flow_Pozos}
+        
+        #supplied TO USERS IN THE supplied AREAS
+        supplied_PP1 = (potabilized_PP1 + aqp4_Toluquilla_to_PP1 + additional_flow_PP1) * (1-loss_grid)
+        supplied_PP2 = (potabilized_PP2 - aqp1_PP2_to_PP3 + additional_flow_PP2) * (1-loss_grid)
+        supplied_PP3 = (potabilized_PP3 + (aqp1_PP2_to_PP3 - aqp2_PP3_to_Pozos) + additional_flow_PP3) * (1-loss_grid)
+        supplied_Toluquilla = (potabilized_Toluquilla + aqp3_Pozos_to_Toluquilla - aqp4_Toluquilla_to_PP1 + additional_flow_Toluquilla) * (1-loss_grid)
+        supplied_Pozos = (potabilized_Pozos + (aqp2_PP3_to_Pozos - aqp3_Pozos_to_Toluquilla) + additional_flow_Pozos) * (1-loss_grid)
+
+        supplied_outputs = {"supplied_PP1": supplied_PP1,"supplied_PP2": supplied_PP2,"supplied_PP3": supplied_PP3,"supplied_Toluquilla": supplied_Toluquilla,"supplied_Pozos": supplied_Pozos}
+
+        total_supplied = supplied_PP1 + supplied_PP2 + supplied_PP3 + supplied_Toluquilla + supplied_Pozos
+
+        model_outputs = {**supplied_outputs, **demand_outputs, **delivered_outputs} #is this necesary?
 
 
         #3. OBJECTIVE FUNCITONS
-        supplied_demand_1 = supply_1/demand1
-        supplied_demand_2 = supply_2/demand2
-        supplied_demand_3 = supply_3/demand3
-        supplied_demand_4 = supply_4/demand4
-        supplied_demand_5 = supply_5/demand5
 
-        #dumb objective func because numpy gives error here
-        min_supplied_demand = supplied_demand_4
+        #3.1 Individual
         
-        #can't get numpy to work here
-        #min_supplied_demand = np.min([supplied_demand_1,supplied_demand_2,supplied_demand_3, supplied_demand_4, supplied_demand_5])
+        #3.1.1 Supplied demand
+        supplied_demand_outcomes = {f"supplied_demand_{ZA}":np.round(supplied_demand(model_outputs,ZA),4) for ZA in ZA_names}
+
+        #3.1.2 Deficit sq
+        deficit_sq_outcomes = {f"deficit_sq_{ZA}":np.round(sq_deficit(model_outputs,ZA),4) for ZA in ZA_names}
+
+        individual_outcomes = {**supplied_demand_outcomes, **deficit_sq_outcomes}
+
+        #3.2 Agreggated
+
+        min_supplied_demand = min(supplied_demand_outcomes.values())
+        min_deficit_sq = min(deficit_sq_outcomes.values())
+
+        aggregated_outcomes = {"min_supplied_demand":min_supplied_demand, "min_deficit_sq":min_deficit_sq}
+
+        
+      
+        return {**model_outputs, **individual_outcomes, **aggregated_outcomes}
 
 
 
-        return {"Total demand":total_demand, "Total supply": total_supply, "supplied_demand_1": supplied_demand_1,"supplied_demand_2": supplied_demand_2,
-                "supplied_demand_3": supplied_demand_3,"supplied_demand_4": supplied_demand_4,"supplied_demand_5": supplied_demand_5
-                "min_supplied_demand":min_supplied_demand}
 
 
-
-def get_MFC_flows():
-        return demand1, demand2, demand3, demand4, demand5, mfc_flow_1, mfc_flow_2, mfc_flow_3, mfc_flow_4, mfc_flow_5
-
-
-
-
-
-'''print({"Supplied demand SA1 =": supplied_demand_1,"Supplied demand SA2 =": supplied_demand_2,"Supplied demand SA3 =": supplied_demand_3,"Supplied demand SA4 =": supplied_demand_4,"Supplied demand SA5 =": supplied_demand_5}
+'''print({"Supplied demand SA1 =": supplied_demand_PP1,"Supplied demand SA2 =": supplied_demand_PP2,"Supplied demand SA3 =": supplied_demand_PP3,"Supplied demand SA4 =": supplied_demand_Toluquilla,"Supplied demand SA5 =": supplied_demand_Pozos}
 )'''
 
 '''              ,cost_loss_a = 500 #million per percentage
@@ -81,13 +146,13 @@ def get_MFC_flows():
               ,cost_rfc = 0.005 #million per m2
                             ,reduction_loss_grid = 0 
               ,reduction_loss_potabilisation = 0
-                            ,rfh_houses_1 = 0, rfh_houses_2 = 0, rfh_houses_3 = 0, rfh_houses_4 = 0
+                            ,rfh_houses_PP1 = 0, rfh_houses_PP2 = 0, rfh_houses_PP3 = 0, rfh_houses_Toluquilla = 0
                             ,water_reuse = 0'''
 
 '''#1.1 Rainfall capture
 conversion_m3s = 30.4 * 24 * 60 / 1000 #converts mm/month to m/s
-rfh_1_flow = (rfh_proportion_1 * private_intakes_1) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
-rfh_2_flow = (rfh_proportion_2 * private_intakes_2) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
-rfh_3_flow = (rfh_proportion_3 * private_intakes_3) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
-rfh_4_flow = (rfh_proportion_4 * private_intakes_4) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
-rfh_5_flow = (rfh_proportion_5 * private_intakes_5) * average_household_area * harvest_coeficient * rainfall * conversion_m3s'''
+rfh_PP1_flow = (rfh_proportion_PP1 * domestic_intakes_PP1) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
+rfh_PP2_flow = (rfh_proportion_PP2 * domestic_intakes_PP2) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
+rfh_PP3_flow = (rfh_proportion_PP3 * domestic_intakes_PP3) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
+rfh_Toluquilla_flow = (rfh_proportion_Toluquilla * domestic_intakes_Toluquilla) * average_household_area * harvest_coeficient * rainfall * conversion_m3s
+rfh_Pozos_flow = (rfh_proportion_Pozos * domestic_intakes_Pozos) * average_household_area * harvest_coeficient * rainfall * conversion_m3s'''
